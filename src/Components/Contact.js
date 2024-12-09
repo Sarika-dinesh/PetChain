d9382c20517d23cf7cd85ac67624a5e2af46375b
Sarika-dinesh <sarikadinesh2913@gmail.com>
2024-12-08 20:40:53 -0800

    Modified Contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,41 +4,41 @@ import { BsTwitter } from "react-icons/bs";
 import { SiLinkedin } from "react-icons/si";
 import { BsYoutube } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
+import ResDB from "../Assets/ResDB.png";
+import UCD from "../Assets/UCDavis.png";
 
 const Contact = () => {
   return (
     <div className="contact-page-wrapper">
-      <h1 className="primary-heading" style={{fontSize: "30px"}} >Have Question In Mind?</h1>
-      <h1 className="primary-heading" style={{fontSize: "30px"}}>Let Us Help You</h1>
+      <h1 className="primary-heading" style={{ fontSize: "30px" }} >Have Question In Mind?</h1>
+      <h1 className="primary-heading" style={{ fontSize: "30px" }}>Let Us Help You</h1>
       <div className="contact-form-container">
         <input type="text" placeholder="Enter your query here" />
         <button className="secondary-button">Submit</button>
       </div>
 
       {/* *FOOTER* */}
-      <div className="footer-section-one">
-        <div className="footer-logo-container">
-          <img src={Logo} alt="" />
+      <div
+        className="footer-section-two"
+        style={{
+          marginTop: "5%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <div className="footer-section-columns" style={{ flex: "1", textAlign: "center" }}>
+          <img src={UCD} alt="" style={{ width: "40%" }} />
         </div>
-        <div className="footer-icons">
-          <BsTwitter />
-          <SiLinkedin />
-          <BsYoutube />
-          <FaFacebookF />
+        <div className="footer-section-columns" style={{ flex: "1", textAlign: "center" }}>
+          <img src={ResDB} alt="" style={{ width: "40%" }} />
         </div>
-      </div>
-      <div className="footer-section-two">
-        <div className="footer-section-columns">
-          <span>Quality</span>
-          <span>Help</span>
-          <span>Share</span>
-        </div>
-        <div className="footer-section-columns">
+        <div className="footer-section-columns" style={{ flex: "1", textAlign: "center" }}>
           <span>244-5333-7783</span>
           <span>petchain@gmail.com</span>
           <span>contact@gmail.com</span>
         </div>
-        <div className="footer-section-columns">
+        <div className="footer-section-columns" style={{ flex: "1", textAlign: "center" }}>
           <span>Terms & Conditions</span>
           <span>Privacy Policy</span>
         </div>
